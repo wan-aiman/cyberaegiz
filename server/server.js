8990a26cfa2e89a689d666be20f1414edf05a6aa
wan-aiman <wanaimanwanibrahim@gmail.com>
2024-10-23 22:16:05 +0800

    Add Docker functionalities to all servers

--- server/server.js
+++ server/server.js
@@ -27,8 +27,8 @@ app.listen(PORT, () => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Failed to connect to MongoDB', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
 
     
