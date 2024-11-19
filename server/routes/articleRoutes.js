3d4926a59e7cae13be81023cdbacbc5f3a34ed40
NazrinIzwan <nazrin.izwan24@gmail.com>
2024-11-19 21:21:11 +0800

    Deleted Unused Files

--- server/routes/articleRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getArticles } = require('../controllers/articleController');
-const router = express.Router();
-
-router.get('/', getArticles); // Handles GET requests to /api/articles
-
-module.exports = router;
